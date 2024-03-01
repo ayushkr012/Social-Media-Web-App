09d03748d0803ab27012bf2cb0631824625f2063
ayushkr012 <bt21cse014@iiitn.ac.in>
2024-03-01 11:15:53 +0530

    file updated

--- client/src/screens/navbar/index.jsx
+++ client/src/screens/navbar/index.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+
+// useDispatch is a hook provided by the React Redux library that allows you to dispatch actions to the Redux store.
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
 
 export default function Navbar() {
   return <div>Navbar</div>;
 }
+
+
+/*
+
+In Material-UI, the sx prop is a shorthand for defining inline styles using the Styled System library
+
+
+
+*/
